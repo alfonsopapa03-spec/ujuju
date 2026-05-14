31ce7006ac96e5fa24385f1051c9e9e082c696a5
alfonsopapa03-spec <alfonsopapa03@gmail.com>
2026-05-13 20:10:30 -0500

    Update ola.py

--- ola.py
+++ ola.py
@@ -17,7 +17,7 @@ st.set_page_config(
 )
 
 # ==================== CREDENCIALES ====================
-SUPABASE_DB_URL = "postgresql://postgres.verwlkgitpllyneqxlao:[YOUR-PASSWORD]@aws-0-us-west-2.pooler.supabase.com:6543/postgres"
+SUPABASE_DB_URL = "postgresql://postgres.verwlkgitpllyneqxlao:Conejito800$@aws-0-us-west-2.pooler.supabase.com:6543/postgres"
 
 # ==================== CATÁLOGO PLACAS / CONDUCTORES ====================
 PLACA_CONDUCTOR = {
